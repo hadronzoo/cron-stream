a1e72fcb9f0994fe4180702eac612c730c2457e0
Joshua Griffith <contact@joshuagriffith.com>
2015-04-17 21:16:13 -0500

    Update manifold

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [manifold "0.1.0-beta12"]
+                 [manifold "0.1.0"]
                  [clj-time "0.9.0"]
                  [org.springframework/spring-context "4.1.6.RELEASE"]])
