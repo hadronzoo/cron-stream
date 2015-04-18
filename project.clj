5d92dedfd3fd2200d8961dfda03cb216c3f6a59f
Joshua Griffith <contact@joshuagriffith.com>
2015-04-17 21:19:33 -0500

    Bump version to 0.2.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.joshuagriffith/cron-stream "0.2.0"
+(defproject com.joshuagriffith/cron-stream "0.2.1"
   :description "Manifold-based cron streams"
   :url "https://github.com/hadronzoo/cron-stream"
   :license {:name "Eclipse Public License"
